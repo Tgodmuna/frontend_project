1d1aa475e9a1a77852c5258346d810d201bb515e
Tgod <aguthankgod@gmail.com>
2023-08-04 00:54:53 +0100

    modified:   src/components/Logo.js 	modified:   src/components/Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import Logo from "./Logo";
+// import Logo from "./Logo";
 import MenuItems from "./Menu_items";
 import Bottom from "./Bottom";
+import styles from ''
 
 const Sidebar = () => {
  
@@ -21,11 +22,23 @@ const Sidebar = () => {
 
   return (
     <div style={sidebarStyle}>
-      
-          <Logo />
-          <MenuItems />
-          <Bottom />
-      
+      {/* logo */}
+      <div style={{ width: 123.49, height: 52, position: "relative" }}>
+        <div className={styles.Logo}>
+          <div
+            className={styles.Logo_icon}
+            id={""}
+            style={{ width: 43.33, height: 43.33, position: "relative" }}>
+            <img src='/assets/Graph.svg' alt='graph' />
+          </div>
+
+          <div className={styles.Logo_Title} id={"Logo_title"}>
+            <p>metrix</p>
+          </div>
+        </div>
+      </div>
+      <MenuItems />
+      <Bottom />
     </div>
   );
 };
